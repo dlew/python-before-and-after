fcb62c5ede31dfe3ebbb7f2a8802fe89ca7e84ac
Daniel Lew <dan@mobiata.com>
2013-04-28 16:02:37 -0500

    Cleaned up code
    The basic process is the same, but now I would feel okay showing the
    code to someone else.
    
    It's also been generalized to some extent; instead of being so movie
    focused, it could now work for any data set (that has a name, vote
    count and rating).

--- combine.py
+++ combine.py
@@ -2,6 +2,13 @@
 
 import csv
 
+##############################################################################
+### INPUT
+##############################################################################
+
+# Expects your standard one-word-per-line word list
+# 
+# Suggested word list: http://directory.fsf.org/wiki/Yawl
 def read_word_list(filename):
 	print("Reading word list from %s" % filename)
 
@@ -11,15 +18,19 @@ def read_word_list(filename):
 		words.append(line.strip())
 	return words
 
-def read_ratings(filename):
-	print("Reading ratings from %s" % filename)
+# Reads the ratings.list file from IMDB's data dumps
+#
+# You can retrieve this file from the IMDB FTP sites,
+# found here: http://www.imdb.com/interfaces
+def read_imdb_ratings(filename, read_movies=True, read_tv=False):
+	print("Reading IMDB ratings from %s" % filename)
 
 	# Determines when we are in the movies section of the file
 	past_start = False
-	read_movies = False
+	read_items = False
 
 	f = open(filename, 'r')
-	movies = []
+	items = []
 	for line in f:
 		# Get rid of the newline
 		line = line[:-1]
@@ -27,21 +38,21 @@ def read_ratings(filename):
 		if line.startswith("MOVIE RATINGS REPORT"):
 			past_start = True
 		elif past_start and line.startswith("New  Distribution  Votes  Rank  Title"):
-			read_movies = True
+			read_items = True
 		elif len(line) == 0:
-			read_movies = False
-		elif read_movies:
+			read_items = False
+		elif read_items:
 			# Skip all TV shows
-			if line.endswith("}") or line.startswith('"'):
+			is_tv = line.endswith("}") or line.startswith('"')
+			if (not read_tv and is_tv) or (not read_movies and not is_tv):
 				continue
 
-			# Still not sure how "new" movies work, but let's find out eventually
+			# Still not sure how "new" items work, but let's find out eventually
 			if not line.startswith(" "):
 				print(line)
 				continue
 
 			split = line.split()
-			distribution = split[0]
 			votes = int(split[1])
 			rating = float(split[2])
 
@@ -58,23 +69,31 @@ def read_ratings(filename):
 			if len(name) == 0:
 				continue
 
-			movies.append({
-				"distribution": distribution,
+			items.append({
 				"votes": votes,
 				"rating": rating,
 				"name": name
 			})
 
-	return movies
+	return items
 
-def filter_movies(movies, votes=0, rating=0):
-	print("Filtering out movies with votes lower than %s and rating less than %s" % (votes, rating))
-	filtered_movies = []
-	for movie in movies:
-		if movie["votes"] > votes and movie["rating"] > rating:
-			filtered_movies.append(movie)
+# Filters out items below a certain threshold votes/rating
+# Good for getting rid of results we won't care about
+def filter_items(items, votes=0, rating=0):
+	print("Filtering out items with votes lower than %s and rating less than %s" % (votes, rating))
 
-	return filtered_movies
+	filtered_items = []
+	for item in items:
+		if item["votes"] > votes and item["rating"] > rating:
+			filtered_items.append(item)
+
+	print("Went from %s to %s items" % (len(items), len(filtered_items)))
+
+	return filtered_items
+
+##############################################################################
+### CALCULATIONS
+##############################################################################
 
 def combine(words, items, min_word_length=4):
 	print("Combining items...")
@@ -99,12 +118,77 @@ def combine(words, items, min_word_length=4):
 
 	return pairs
 
+# Retrieves all items that match the text in a trie
+def get_items(node, text):
+	if len(text) == 0:
+		return node["items"]
+
+	search = text[0].lower()
+	if search in node:
+		return get_items(node[search], text[1:])
+	else:
+		return None
+
+# Trie format: has a key for each letter, plus 
+# a key "items" which links to all items matching
+def generate_trie(items, suffix=True, words=[]):
+	root = {}
+
+	# If words were provided, then only accept text where it's
+	# in the dictionary
+	dictionary = {}
+	for word in words:
+		dictionary[word] = True
+		dictionary[word[::-1]] = True
+
+	for item in items:
+		split = item["name"].split()
+		text = ""
+
+		# We only use the first or last word (no point in comparing)
+		# past a space for a before & after
+		if suffix:
+			text = split[-1][::-1]
+		else:
+			text = split[0]
+
+		if text.lower() in dictionary:
+			add_to_trie(root, text, item)
+
+	return root
+
+# Recursive method for adding text to a trie.
+#
+# For this use case, we add the item to each node
+# in the trie (so that we match on substrings)
+def add_to_trie(node, text, item):
+	if len(text) == 0:
+		return
+
+	to_add = text[0].lower()
+	next_node = None
+	if to_add not in node:
+		next_node = { "items" : [] }
+		node[to_add] = next_node
+	else:
+		next_node = node[to_add]
+
+	next_node["items"].append(item)
+
+	add_to_trie(next_node, text[1:], item)
+
+##############################################################################
+### OUTPUT
+##############################################################################
+
+# A comparator for sorting results
+#
 # We rank pairs on two values: minimum votes and a weighted difference in
 # rating.
 #
 # The reason we use minimum votes is so that we have a baseline popularity
-# in both movies.  Average is bad, because a highly voted movie can bring
-# up a bunch of unknown movies.
+# in both items.  Average is bad, because a highly voted movie can bring
+# up a bunch of unknown items.
 #
 # Difference in rating is based on the idea that pairing a good movie with
 # a bad movie is hilarious.
@@ -126,6 +210,7 @@ def pair_compare(pair1, pair2):
 	else:
 		return 0
 
+# Simple method for printing results
 def print_pairs(pairs):
 	for pair in pairs:
 		word = pair[0]
@@ -134,13 +219,13 @@ def print_pairs(pairs):
 		vote_avg = (pair[1]["votes"] + pair[2]["votes"]) / 2
 		vote_min = min(pair[1]["votes"], pair[2]["votes"])
 		rating_diff = abs(pair[1]["rating"] - pair[2]["rating"])
-		print("%s (%s, %s): %s%s" % (word, rating_diff, vote_min, one["name"], two["name"][len(word):]))
+		print("%s (%s, %s): %s" % (word, rating_diff, vote_min, combine_pair(pair)))
 
-def combine_pair(pair):
-	return "%s%s" % (pair[1]["name"], pair[2]["name"][len(pair[0]):])
+# This is good for exporting the data
+def write_pairs_csv(filename, pairs):
+	print("Writing pairs file to %s" % filename)
 
-def write_csv(pairs):
-	writer = csv.writer(open("pairs.csv", "wb"))
+	writer = csv.writer(open(filename, "wb"))
 
 	# Header
 	writer.writerow([
@@ -167,69 +252,20 @@ def write_csv(pairs):
 			pair[2]["rating"],
 		])
 
+# Combines two items into one glorious before & after name
+def combine_pair(pair):
+	return "%s%s" % (pair[1]["name"], pair[2]["name"][len(pair[0]):])
 
-def get_items(node, text):
-	if len(text) == 0:
-		return node["items"]
+##############################################################################
+### MAIN
+##############################################################################
 
-	search = text[0].lower()
-	if search in node:
-		return get_items(node[search], text[1:])
-	else:
-		return None
-
-# Trie format: has a key for each letter, plus 
-# a key "items" which links to all items matching
-def generate_trie(items, suffix=True, words=[]):
-	root = {}
-
-	# If words were provided, then only accept text where it's
-	# in the dictionary
-	dictionary = {}
-	for word in words:
-		dictionary[word] = True
-		dictionary[word[::-1]] = True
-
-	for item in items:
-		split = item["name"].split()
-		text = ""
-		if suffix:
-			text = split[-1][::-1]
-		else:
-			text = split[0]
-
-		if text.lower() in dictionary:
-			add_to_trie(root, text, item)
-
-	return root
-
-def add_to_trie(node, text, item):
-	if len(text) == 0:
-		return
-
-	to_add = text[0].lower()
-	next_node = None
-	if to_add not in node:
-		next_node = { "items" : [] }
-		node[to_add] = next_node
-	else:
-		next_node = node[to_add]
-
-	next_node["items"].append(item)
-
-	add_to_trie(next_node, text[1:], item)
-
+# TODO: This could be command-line-ized but I'm being lazy and just hardcoding
+# the process here.
 if __name__ == "__main__":
 	words = read_word_list("word.list")
-	movies = read_ratings("ratings.list")
-
-	filtered_movies = filter_movies(movies, 5000, 0)
-
-	pairs = combine(words, filtered_movies, 4)
-	pairs = sorted(pairs, cmp=pair_compare)
+	items = read_imdb_ratings("ratings.list")
+	filtered_items = filter_items(items, 5000, 0)
+	pairs = sorted(combine(words, filtered_items, 4), cmp=pair_compare)
 	print_pairs(pairs)
-	write_csv(pairs)
-
-	# print("# movies: %s, # filtered movies: %s" % (len(movies), len(filtered_movies)))
-
-	#brute_force_combine(words, filtered_movies)
+	write_pairs_csv("pairs.csv", pairs)
